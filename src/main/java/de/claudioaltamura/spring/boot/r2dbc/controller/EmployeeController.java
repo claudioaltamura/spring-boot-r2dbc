239250b9fa4011da7ee05553f848d9d7b80767a7
Claudio Altamura <claudio@claudioaltamura.de>
2023-11-13 18:03:16 +0100

    feat(employee): added search by name

--- src/main/java/de/claudioaltamura/spring/boot/r2dbc/controller/EmployeeController.java
+++ src/main/java/de/claudioaltamura/spring/boot/r2dbc/controller/EmployeeController.java
@@ -6,10 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
@@ -23,6 +20,13 @@ public class EmployeeController {
     @GetMapping(path = "/employees")
     public Mono<ResponseEntity<List<Employee>>> getEmployees() {
         return employeeRepository.findAll()
+                .collectList()
+                .map(employees -> new ResponseEntity<>(employees, HttpStatus.OK));
+    }
+
+    @GetMapping(path = "/employees/search")
+    public Mono<ResponseEntity<List<Employee>>> findByName(@RequestParam String name) {
+        return employeeRepository.findByName(name)
                 .collectList()
                 .map(employees -> new ResponseEntity<>(employees, HttpStatus.OK));
     }
--- src/main/java/de/claudioaltamura/spring/boot/r2dbc/repository/EmployeeRepository.java
+++ src/main/java/de/claudioaltamura/spring/boot/r2dbc/repository/EmployeeRepository.java
@@ -1,9 +1,14 @@
 package de.claudioaltamura.spring.boot.r2dbc.repository;
 
 import de.claudioaltamura.spring.boot.r2dbc.model.Employee;
+import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Flux;
 
 import java.util.UUID;
 
 public interface EmployeeRepository extends ReactiveCrudRepository<Employee, UUID> {
+
+    @Query("select id, first_name, last_name from employee where first_name = $1")
+    Flux<Employee> findByName(String name);
 }
